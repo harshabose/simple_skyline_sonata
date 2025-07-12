ae10e7491f210e3cf0478cfc124ae91c1596bab0
harshabose <hbvardhan@gmail.com>
2025-07-12 18:18:45 +0530

    Re-add tools submodule properly

--- dependencies/tools/go.mod
+++
@@ -1,7 +0,0 @@
-module github.com/harshabose/tools
-
-go 1.24.1
-
-require github.com/asticode/go-astiav v0.37.0
-
-require github.com/asticode/go-astikit v0.42.0 // indirect
--- dependencies/tools/pkg/buffer/errors.go
+++
@@ -1,8 +0,0 @@
-package buffer
-
-import "errors"
-
-var (
-	ErrorElementUnallocated = errors.New("encountered nil in the buffer. this should not happen. check usage")
-	ErrorChannelBufferClose = errors.New("channel buffer has be closed. cannot perform this operation")
-)
